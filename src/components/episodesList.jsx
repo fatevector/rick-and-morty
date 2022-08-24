fdad08ce82611b95251fa49a9f094cdbade5237a
Ivan Petrov <fatevector@yandex.ru>
2022-08-24 21:11:57 +0300

    made EpisodesList component

--- src/components/episodesList.jsx
+++ src/components/episodesList.jsx
@@ -1,5 +1,21 @@
+import { episodes } from "../fakeStorage/episodes";
+
 const EpisodesList = () => {
-    return <div>EpisodesList</div>;
+    return (
+        <div className="container">
+            <div className="row">
+                {episodes.map((episode) => (
+                    <div
+                        key={episode.id}
+                        style={{ height: "200px" }}
+                        className="col-3"
+                    >
+                        {episode.name}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default EpisodesList;
