12b3b94cd9476298928c73d079ec14fdbfe820fc
Ivan Petrov <fatevector@yandex.ru>
2022-08-24 21:38:41 +0300

    made Episode component

--- src/components/episodesList.jsx
+++ src/components/episodesList.jsx
@@ -1,17 +1,12 @@
 import { episodes } from "../fakeStorage/episodes";
+import Episode from "./episode";
 
 const EpisodesList = () => {
     return (
         <div className="container">
             <div className="row">
                 {episodes.map((episode) => (
-                    <div
-                        key={episode.id}
-                        style={{ height: "200px" }}
-                        className="col-3"
-                    >
-                        {episode.name}
-                    </div>
+                    <Episode key={episode.id} {...episode} />
                 ))}
             </div>
         </div>
