dd25f223922c7823620dd1d7356f0ab0874c2bed
Ivan Petrov <fatevector@yandex.ru>
2022-08-27 00:11:53 +0300

    init Pagination component

--- src/components/episodesList.jsx
+++ src/components/episodesList.jsx
@@ -1,13 +1,28 @@
 import { episodes } from "../fakeStorage/episodes";
 import Episode from "./episode";
+import Pagination from "./pagination";
 
 const EpisodesList = () => {
+    const count = episodes.length;
+    const pageSize = 8;
+
+    const handlePageChange = (pageIndex) => {
+        console.log(pageIndex);
+    };
+
     return (
         <div className="container">
             <div className="row">
                 {episodes.map((episode) => (
                     <Episode key={episode.id} {...episode} />
                 ))}
+            </div>
+            <div className="row">
+                <Pagination
+                    itemsCount={count}
+                    pageSize={pageSize}
+                    onPageChange={handlePageChange}
+                />
             </div>
         </div>
     );
